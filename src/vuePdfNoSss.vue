0439a711b34eb72d78d05929b81c372cd181a854
Gema Aji Wardian <gema_wardian@hotmail.com>
2024-02-13 22:57:54 +0700

    Upgrade core pdf deps to support Webpack 5

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,24 +1,17 @@
 <style src="./annotationLayer.css"></style>
 <script>
+import componentFactory from "./componentFactory";
+import pdfjsWrapper from "./pdfjsWrapper";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
-	import componentFactory from './componentFactory.js'
+var PDFJS = require("pdfjs-dist");
+if (process.env.VUE_ENV !== "server") {
+  if (typeof window !== "undefined" && "Worker" in window) {
+    PDFJS.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+  }
+}
 
-	if ( process.env.VUE_ENV !== 'server' ) {
-
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
-
-		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
-
-		var component = componentFactory(pdfjsWrapper(PDFJS));
-	} else {
-
-		var component = componentFactory({});
-	}
-
-	export default component;
+var component = componentFactory(pdfjsWrapper(PDFJS));
+component.PDFJS = PDFJS;
+export default component;
 </script>
